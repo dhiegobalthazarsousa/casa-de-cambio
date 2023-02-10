4b18faf031f6eac02b3aa613468a531404beb0a1
Dhiego Balthazar <dhiegobalthazar@gmail.com>
2023-02-10 15:20:08 -0300

    Feat: get api

--- src/script.js
+++ src/script.js
@@ -1,2 +1,40 @@
 import './style.css';
+import Swal from 'sweetalert2';
 
+const URL_API_MOEDA = 'https://api.exchangerate.host/latest?base=';
+
+const btnElement = document.querySelector("#search");
+const inputElement = document.querySelector("#coin");
+const responseElement = document.querySelector("#response");
+
+const searchCoin = (coin, callback) => {
+    fetch(`${URL_API_MOEDA}${coin}`)
+    .then((response) => response.json())
+    .then((data) => {
+        //console.log(data);
+        callback(data);
+    });
+};
+
+const pesquisarListener = () => {
+    btnElement.addEventListener('click', (event) => {
+        event.preventDefault();
+        responseElement.innerHTML = '';
+        const value = inputElement.value;
+        searchCoin(value, generateResult);
+    });
+}
+
+const generateResult = ({ rates }) => {
+    const coins = Object.keys(rates);
+    console.log(rates);
+    coins.forEach((coin) => {
+        const divElement = document.createElement('div');
+        divElement.innerHTML = `${coin} ${rates[coin]}`;
+        responseElement.appendChild(divElement);
+    });
+};
+
+window.onload = () => {
+    pesquisarListener();
+}
